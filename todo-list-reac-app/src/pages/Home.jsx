5dcec70c1cfd3ed74c4138d06c28ec3d4042d475
Mateus Powell <mkabyz@gmail.com>
2022-04-25 21:42:15 -0300

    ih

--- todo-list-reac-app/src/pages/Home.jsx
+++ todo-list-reac-app/src/pages/Home.jsx
@@ -1,24 +1,53 @@
-import { TodoListItem } from '../components/TodoListItem'
-import './Home.css'
+import {  useState } from "react";
+import { TodoListItem } from "../components/TodoListItem";
+import "./Home.css";
 
 export function Home() {
-    const itemsArray = [
-        { id: 1, text: "Acordar 7 horas da manhã", checked: false},
-        { id: 2, text: "Lavar toda a louça suja que está dentro ou fora da pia", checked: false},
-        { id: 3, text: "Levar os cachorros para passear por 30 min", checked: false},
-        { id: 4, text: "Lavar o carro todo", checked: true},
+  const [itemsArray, setItemsArray] = useState(() => {
+    let array = [
+      { id: 1, text: "Acordar 7 horas da manhã", checked: false },
+      {
+        id: 2,
+        text: "Lavar toda a louça suja que está dentro ou fora da pia",
+        checked: false,
+      },
+      {
+        id: 3,
+        text: "Levar os cachorros para passear por 30 min",
+        checked: false,
+      },
+      { id: 4, text: "Lavar o carro todo", checked: true },
     ];
+    console.log(localStorage.getItem("itemsArray"));
+    const storedArray = localStorage.getItem("itemsArray");
+    if (storedArray != null) {
+      array = JSON.parse.storedArray;
+    } else {
+      const itemsArrayString = JSON.stringify(array);
+      localStorage.setItem("itemsArray", itemsArrayString);
+    }
+  });
 
-    // localStorage.setItem("abc", "oi, tudo bem?");
-    // sessionStorage.setItem("def", "não, ta tudo uma bela bosta!");
-    console.log(itemsArray);
-
-    return (
-        <div className="container">
-            {itemsArray.map(({id, text, checked}) => {
-                {/* return <TodoListItem {...obj} /> */}
-                return <TodoListItem id={id} text={text} key={id} checked={checked}/>
-            })}
-        </div>
-    )
+  function updateChecked(id, checked) {
+    const buscaIndex = itemsArray.findIndex((item) => {
+      return item.id === id;
+    });
+    itemsArray[buscaIndex].checked = checked;
+    setItemsArray(JSON.stringify.storedArray);
+  }
+  return (
+    <div className="container">
+      {itemsArray?.map(({ id, text, checked }) => {
+        return (
+          <TodoListItem
+            id={id}
+            text={text}
+            key={id}
+            checked={checked}
+            updateChecked={updateChecked}
+          />
+        );
+      })}
+    </div>
+  );
 }
